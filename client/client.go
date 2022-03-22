638c7dd5e1828c587aae78503c264690553a9ce2
IntrntSrfr <aleksander@tosbakken.com>
2022-03-22 20:41:54 +0100

    move client functinality

--- client/client.go
+++ client/client.go
@@ -1,34 +1,47 @@
 package client
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/x509"
+	"encoding/binary"
 	"encoding/json"
 	"encoding/pem"
-	"github.com/intrntsrfr/gonion"
+	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"net/netip"
 	"net/url"
+
+	"github.com/intrntsrfr/gonion"
+	"github.com/intrntsrfr/gonion/packet"
 )
 
-func ParseRequest(inp string) *gonion.HTTPRequest {
-	u, err := url.Parse(inp)
+// NodeCount will be 3 for this client.
+const NodeCount = 3
+const MaxContent = 256
+const DirURL = "http://localhost:9051/api/nodes"
+
+type Client struct {
+	directoryURL string
+	nodes        []*gonion.NodeInfo
+	secrets      []string
+}
+
+func marshalRequest(req *gonion.HTTPRequest) []byte {
+	d, err := json.Marshal(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return &gonion.HTTPRequest{
-		Method:  "GET",
-		Scheme:  u.Scheme,
-		Host:    u.Host,
-		Path:    u.Path,
-		Queries: u.Query(),
-	}
+	return d
 }
 
-func GetNodes() []*gonion.NodeInfo {
-	res, err := http.DefaultClient.Get("http://localhost:9051/api/nodes")
+func (cli *Client) getNodes() []*gonion.NodeInfo {
+	res, err := http.DefaultClient.Get(cli.directoryURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,7 +57,137 @@ func GetNodes() []*gonion.NodeInfo {
 	if err != nil {
 		log.Fatal(err)
 	}
+	cli.nodes = nodes
 	return nodes
+}
+func Do(req *gonion.HTTPRequest) *bytes.Buffer {
+	c := &Client{directoryURL: DirURL}
+	return c.do(req)
+}
+
+func (cli *Client) do(req *gonion.HTTPRequest) *bytes.Buffer {
+
+	inner := bytes.NewBuffer(marshalRequest(req))
+
+	// first we must get the nodes and their public keys
+	nodes := cli.getNodes()
+
+	if len(nodes) < 3 {
+		log.Fatal("too few nodes")
+	}
+
+	node1 := nodes[0]
+	// dont need a layer for node 1 as its directly connected
+	c, err := net.Dial("tcp", fmt.Sprintf("%v:%v", node1.IP, node1.Port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closeConn(c)
+
+	// before sending request, we need to get secrets from each node
+
+	// partition the body into sizes we want to manage
+	// send each packet separately to the next node
+	for {
+		partialMsg := make([]byte, MaxContent)
+		n, err := inner.Read(partialMsg)
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+		p := packet.NewPacket()
+		p.AddDataFrame(partialMsg[:n], n != MaxContent)
+		//fmt.Println("before encrypt")
+		//p.PrintInfo()
+
+		// packet should be encrypted here with node 3 key
+		p.AESEncrypt([]byte("siggarett"))
+
+		// here we add the layers for the 2 other nodes
+
+		// add 2x relay header
+		for i := 2; i > 0; i-- {
+			node := nodes[i]
+			ap, err := netip.ParseAddrPort(fmt.Sprintf("%v:%v", node.IP, node.Port))
+			if err != nil {
+				log.Fatal(err)
+			}
+			nodeIP := ap.Addr().As4()
+			nodePort := ap.Port()
+			p.AddRelayFrame(nodeIP, [2]byte{byte((nodePort & 0xff00) >> 8), byte(nodePort & 0xff)}, n != MaxContent)
+
+			// it should be a layer of encryption here
+			p.AESEncrypt([]byte("siggarett"))
+		}
+
+		p.Pad()
+		_, err = c.Write(p.Bytes())
+		if err != nil {
+			log.Fatal(err)
+		}
+		if n != MaxContent {
+			break
+		}
+	}
+
+	fmt.Println("i will now try to read a response")
+
+	resp := new(bytes.Buffer)
+
+	for {
+		fmt.Println("receiving packet...")
+		tmp := make([]byte, packet.MaxPacketSize)
+		fmt.Println("read 1")
+		_, err := c.Read(tmp)
+		fmt.Println("read 2")
+		if err != nil {
+			fmt.Println(err)
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		p := packet.NewPacketFromBytes(tmp)
+		p.Trim()
+
+		// we need to 3x decrypt here
+
+		header := p.PopBytes(2)
+		length := int(binary.BigEndian.Uint16(p.PopBytes(2)))
+		resp.Write(p.Bytes()[:length])
+		//io.Copy(f, bytes.NewBuffer(p.Bytes()[:length]))
+		if header[0]&1 == 1 {
+			break
+		}
+	}
+
+	return resp
+}
+
+func closeConn(c net.Conn) {
+	err := c.Close()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("closed connection")
+
+}
+
+func (c *Client) GetSecret() {
+
+}
+
+func ParseURL(inp string) *gonion.HTTPRequest {
+	u, err := url.Parse(inp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &gonion.HTTPRequest{
+		Method:  "GET",
+		Scheme:  u.Scheme,
+		Host:    u.Host,
+		Path:    u.Path,
+		Queries: u.Query(),
+	}
 }
 
 /*
@@ -106,25 +249,4 @@ func BytesToPublicKey(pub []byte) *rsa.PublicKey {
 		log.Fatal("not ok")
 	}
 	return key
-}
-
-func fireRequest() {
-	// get the nodes from the node directory
-
-	// get secret for first node
-
-	// get secret for second node
-
-	// get secret for last node
-
-	// create the request
-
-	// encrypt request with 3 layers from public keys
-
-	// fire request to the first node
-
-	// wait for a response
-
-	// decrypt response using the 3 node secrets
-
 }
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,137 +1,41 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"log"
-	"net"
-	"net/netip"
 	"os"
 
 	"github.com/intrntsrfr/gonion/client"
-	"github.com/intrntsrfr/gonion/packet"
 )
-
-func closeConn(c net.Conn) {
-	err := c.Close()
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("closed connection")
-
-}
-
-//const MaxContent = packet.MaxPacketSize - (NodeCount-1)*8 - 4
-const MaxContent = 256
-const NodeCount = 3
 
 func main() {
 
+
 	flag.Parse()
+
+	fmt.Println(flag.Args())
+
+	return
 	file := flag.Arg(0)
 	if file == "" {
 		fmt.Println("please specify an output file")
 		return
 	}
 
-	a := "https://www.vg.no/"
-	req := client.ParseRequest(a)
+	return
 
-	d, _ := json.Marshal(req)
-	innerMsgBuffer := bytes.NewBuffer(d)
-	log.Println("msg buffer length:", len(d))
+	a := "http://eu.httpbin.org/flasgger_static/swagger-ui-bundle.js"
+	req := client.ParseURL(a)
 
-	// first we must get the nodes and their public keys
-	nodes := client.GetNodes()
-
-	if len(nodes) < 3 {
-		log.Fatal("too few nodes")
-	}
-
-	node1 := nodes[0]
-	// dont need a layer for node 1 as its directly connected
-	c, err := net.Dial("tcp", fmt.Sprintf("%v:%v", node1.IP, node1.Port))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer closeConn(c)
-
-	// partition the body into sizes we want to manage
-	// send each packet separately to the next node
-	for {
-		partialMsg := make([]byte, MaxContent)
-		n, err := innerMsgBuffer.Read(partialMsg)
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
-		}
-		p := packet.NewPacket()
-		p.AddDataFrame(partialMsg[:n], n != MaxContent)
-
-		// packet should be encrypted here with node 3 key
-		//p.AESEncrypt([]byte("siggarett"))
-
-		// here we add the layers for the 2 other nodes
-
-		// the loop body can probably be turned into a function call instead, making it much easier to deal with
-		// when it comes to encryption
-
-		// add 2x relay header
-		for i := 2; i > 0; i-- {
-			node := nodes[i]
-			ap, err := netip.ParseAddrPort(fmt.Sprintf("%v:%v", node.IP, node.Port))
-			if err != nil {
-				log.Fatal(err)
-			}
-			nodeIP := ap.Addr().As4()
-			nodePort := ap.Port()
-			p.AddRelayFrame(nodeIP, [2]byte{byte((nodePort & 0xff00) >> 8), byte(nodePort & 0xff)}, n != MaxContent)
-
-			// it should be a layer of encryption here
-			//p.AESEncrypt([]byte("siggarett"))
-		}
-
-		p.Pad()
-		//p.PrintInfo()
-		_, err = c.Write(p.Bytes())
-		if err != nil {
-			log.Fatal(err)
-		}
-		if n != MaxContent {
-			break
-		}
-	}
-
-	fmt.Println("i will now try to read a response")
-
-	// read the response
-	resp := new(bytes.Buffer)
-	for {
-		fmt.Println("receiving packet...")
-		tmp := make([]byte, packet.MaxPacketSize)
-		_, err := c.Read(tmp)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatal(err)
-		}
-		p := packet.NewPacketFromBytes(tmp)
-		//p.PrintInfo()
-		header := p.PopBytes(2)
-		length := int(binary.BigEndian.Uint16(p.PopBytes(2)))
-		resp.Write(p.Bytes()[:length])
-		if header[0]&1 == 1 {
-			break
-		}
-	}
+	resp := client.Do(req)
 
 	f, err := os.Create(file)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
+
 	io.Copy(f, resp)
 }
